8e968ae3878c64d518a001c20794f1c3d015e60c
Begeassgit <begeass@sina.com>
2020-07-10 17:04:01 +0800

    UpDate

--- src/main/java/com/repair/web/Controller/AE/CarsAEController.java
+++ src/main/java/com/repair/web/Controller/AE/CarsAEController.java
@@ -3,14 +3,24 @@ package com.repair.web.Controller.AE;/*
 */
 
 
+import com.repair.web.Entity.Cars;
 import com.repair.web.Service.AE.CarAEService;
 import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -59,4 +69,74 @@ public class CarsAEController {
         modelAndView.addAllObjects(map);
         return modelAndView;
     }
+
+    @RequestMapping(value = "/AddOneCar",method = RequestMethod.POST)
+    public ModelAndView addOneCar(Cars cars){
+        ModelAndView modelAndView=new ModelAndView();
+        Map<String,Object> map=new HashMap<>();
+        if(!carAEService.addOneCar(cars)){
+            modelAndView.setViewName("Error");
+        }
+        modelAndView.setViewName("BaseTraffic");
+        map.put("cars",carAEService.getAllCarInfo(cars.getCars_company()));
+        map.put("company",cars.getCars_company());
+        modelAndView.addAllObjects(map);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/OutputCarSheet",method = RequestMethod.POST)
+    public void outputCarSheet(HttpServletResponse response, String company){
+        XSSFWorkbook xssfWorkbook=carAEService.outputCarSheet(company);
+        String fileName="交通载具.xlsx";
+        OutputStream outputStream;
+        try{
+            fileName= URLEncoder.encode(fileName,"UTF-8");
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-disposition","attachment;filename="+fileName);
+            outputStream=response.getOutputStream();
+            xssfWorkbook.write(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (IOException eio){
+            eio.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping(value = "/OutputCarSheetTemplate",method = RequestMethod.POST)
+    public void outputCarSheet(HttpServletResponse response){
+        XSSFWorkbook xssfWorkbook=carAEService.outputCarSheetTemplate();
+        String fileName="交通载具模板.xlsx";
+        OutputStream outputStream;
+        try{
+            fileName= URLEncoder.encode(fileName,"UTF-8");
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-disposition","attachment;filename="+fileName);
+            outputStream=response.getOutputStream();
+            xssfWorkbook.write(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (IOException eio){
+            eio.printStackTrace();
+        }
+
+    }
+
+    @RequestMapping(value = "/uploadCars",method = RequestMethod.POST)
+    public ModelAndView uploadCar(@RequestParam("file") MultipartFile file, String company) {
+        ModelAndView modelAndView = new ModelAndView();
+        Map<String, Object> map = new HashMap<>();
+        if (!carAEService.uploadCar(file)) {
+            modelAndView.setViewName("Error");
+        } else {
+            map.put("company", company);
+            map.put("cars", carAEService.getAllCarInfo(company));
+            modelAndView.addAllObjects(map);
+        }
+        return modelAndView;
+    }
+
+    
 }
--- src/main/java/com/repair/web/Service/AE/CarAEService.java
+++ src/main/java/com/repair/web/Service/AE/CarAEService.java
@@ -4,9 +4,20 @@ package com.repair.web.Service.AE;/*
 
 import com.repair.web.Dao.CarsDao;
 import com.repair.web.Entity.Cars;
+import com.repair.web.Entity.Device;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,5 +39,89 @@ public class CarAEService {
 
     public boolean borrowCarService(String cars_id,String cars_company,String cars_department){
         return carsDao.borrowOut(cars_id, cars_company, cars_department)>=1;
+    }
+
+    public boolean addOneCar(Cars cars){
+        return carsDao.addCar(cars)>=1;
+    }
+
+    public XSSFWorkbook outputCarSheet(String company){
+        XSSFWorkbook xssfWorkbook=new XSSFWorkbook();
+        List<Cars> list=carsDao.getAllCar(company);
+        Sheet sheet=xssfWorkbook.createSheet("交通载具");
+        Row titleRow=sheet.createRow(0);
+        titleRow.createCell(0).setCellValue("车辆编号");
+        titleRow.createCell(1).setCellValue("车辆型号");
+        titleRow.createCell(2).setCellValue("车辆品牌");
+        titleRow.createCell(3).setCellValue("状态");
+        titleRow.createCell(4).setCellValue("使用部门");
+        titleRow.createCell(5).setCellValue("所属公司");
+        int cell=1;
+        for(Cars cars:list){
+            Row row=sheet.createRow(cell);
+            row.createCell(0).setCellValue(cars.getCars_id());
+            row.createCell(1).setCellValue(cars.getCars_name());
+            row.createCell(2).setCellValue(cars.getCars_brand());
+            row.createCell(3).setCellValue(cars.getCars_status());
+            row.createCell(4).setCellValue(cars.getCars_department());
+            row.createCell(5).setCellValue(cars.getCars_company());
+            cell++;
+        }
+        return xssfWorkbook;
+    }
+
+    public XSSFWorkbook outputCarSheetTemplate(){
+        XSSFWorkbook xssfWorkbook=new XSSFWorkbook();
+        List<Cars> list=new ArrayList<>();
+        Sheet sheet=xssfWorkbook.createSheet("交通载具-模板");
+        Row titleRow=sheet.createRow(0);
+        titleRow.createCell(0).setCellValue("车辆编号");
+        titleRow.createCell(1).setCellValue("车辆型号");
+        titleRow.createCell(2).setCellValue("车辆品牌");
+        titleRow.createCell(3).setCellValue("状态");
+        titleRow.createCell(4).setCellValue("使用部门");
+        titleRow.createCell(5).setCellValue("所属公司");
+        int cell=1;
+        for(Cars cars:list){
+            Row row=sheet.createRow(cell);
+            row.createCell(0).setCellValue(cars.getCars_id());
+            row.createCell(1).setCellValue(cars.getCars_name());
+            row.createCell(2).setCellValue(cars.getCars_brand());
+            row.createCell(3).setCellValue(cars.getCars_status());
+            row.createCell(4).setCellValue(cars.getCars_department());
+            row.createCell(5).setCellValue(cars.getCars_company());
+            cell++;
+        }
+        return xssfWorkbook;
+    }
+
+    public boolean uploadCar(MultipartFile file){
+        int flag=0;
+        try{
+            XSSFWorkbook book=new XSSFWorkbook(file.getInputStream());
+            DataFormatter dataFormatter=new DataFormatter();
+            int sheetNum=book.getNumberOfSheets();
+            for(int i=0;i<sheetNum;i++){
+                XSSFSheet sheet=book.getSheetAt(i);
+                int rows=sheet.getPhysicalNumberOfRows();
+                Device device=null;
+                for(int j=1;j<rows;j++){
+                    Cars cars=new Cars();
+                    XSSFRow xssfRow=sheet.getRow(j);
+                    cars.setCars_id(dataFormatter.formatCellValue(xssfRow.getCell(0)));
+                    cars.setCars_name(dataFormatter.formatCellValue(xssfRow.getCell(1)));
+                    cars.setCars_brand(dataFormatter.formatCellValue(xssfRow.getCell(2)));
+                    cars.setCars_status(dataFormatter.formatCellValue(xssfRow.getCell(3)));
+                    cars.setCars_department(dataFormatter.formatCellValue(xssfRow.getCell(4)));
+                    cars.setCars_company(dataFormatter.formatCellValue(xssfRow.getCell(5)));
+                    flag=carsDao.addCar(cars);
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return flag>=1;
+
     }
 }
