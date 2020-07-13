b362a9c2223cd5bd2a1fcb550e8f53dfded1a22f
Begeassgit <begeass@sina.com>
2020-07-13 17:13:22 +0800

    Function fix

--- src/main/java/com/repair/web/Controller/AE/FurnitureAEController.java
+++ src/main/java/com/repair/web/Controller/AE/FurnitureAEController.java
@@ -3,11 +3,16 @@ package com.repair.web.Controller.AE;/*
 */
 
 import com.repair.web.Service.AE.FurnitureAEService;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,5 +34,43 @@ public class FurnitureAEController {
         modelAndView.addAllObjects(map);
 
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/OutputFurniture",method = RequestMethod.POST)
+    public void outputFurniture(HttpServletResponse response,String company){
+        XSSFWorkbook xssfWorkbook=furnitureAEService.outputFurnitureSheet(company);
+        OutputStream outputStream;
+        String fileName="交通载具.xlsx";
+        try{
+            fileName= URLEncoder.encode(fileName,"UTF-8");
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-disposition","attachment;filename="+fileName);
+            outputStream=response.getOutputStream();
+            xssfWorkbook.write(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (IOException eio){
+            eio.printStackTrace();
+        }
+    }
+
+    @RequestMapping(value = "/OutputFurnitureTemplate",method = RequestMethod.GET)
+    public void outputFurniture(HttpServletResponse response){
+        XSSFWorkbook xssfWorkbook=furnitureAEService.outputFurnitureSheetTemplate();
+        OutputStream outputStream;
+        String fileName="交通载具模板.xlsx";
+        try{
+            fileName= URLEncoder.encode(fileName,"UTF-8");
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("Content-disposition","attachment;filename="+fileName);
+            outputStream=response.getOutputStream();
+            xssfWorkbook.write(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (IOException eio){
+            eio.printStackTrace();
+        }
     }
 }
--- src/main/java/com/repair/web/Service/AE/FurnitureAEService.java
+++ src/main/java/com/repair/web/Service/AE/FurnitureAEService.java
@@ -3,9 +3,14 @@ package com.repair.web.Service.AE;/*
 */
 
 import com.repair.web.Dao.FurnitureDao;
+import com.repair.web.Entity.Cars;
 import com.repair.web.Entity.Furniture;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,5 +23,55 @@ public class FurnitureAEService {
 
     public List<Furniture> getAllFurniture(String company){
         return furnitureDao.getAllFurniture(company);
+    }
+
+    public XSSFWorkbook outputFurnitureSheet(String company){
+        XSSFWorkbook xssfWorkbook=new XSSFWorkbook();
+        List<Furniture> list=furnitureDao.getAllFurniture(company);
+        Sheet sheet=xssfWorkbook.createSheet("家具用具");
+        Row titleRow=sheet.createRow(0);
+        titleRow.createCell(0).setCellValue("编号");
+        titleRow.createCell(1).setCellValue("家具用具类型");
+        titleRow.createCell(2).setCellValue("家具用具名字");
+        titleRow.createCell(3).setCellValue("家具用具品牌");
+        titleRow.createCell(4).setCellValue("家具用具余量");
+        titleRow.createCell(5).setCellValue("所属公司");
+        int cell=1;
+        for(Furniture furniture:list){
+            Row row=sheet.createRow(cell);
+            row.createCell(0).setCellValue(furniture.getFurniture_id());
+            row.createCell(1).setCellValue(furniture.getFurniture_type());
+            row.createCell(2).setCellValue(furniture.getFurniture_name());
+            row.createCell(3).setCellValue(furniture.getFurniture_brand());
+            row.createCell(4).setCellValue(furniture.getFurniture_count());
+            row.createCell(5).setCellValue(furniture.getFurniture_company());
+            cell++;
+        }
+        return xssfWorkbook;
+    }
+
+    public XSSFWorkbook outputFurnitureSheetTemplate(){
+        XSSFWorkbook xssfWorkbook=new XSSFWorkbook();
+        List<Furniture> list=new ArrayList<>();
+        Sheet sheet=xssfWorkbook.createSheet("家具用具");
+        Row titleRow=sheet.createRow(0);
+        titleRow.createCell(0).setCellValue("编号");
+        titleRow.createCell(1).setCellValue("家具用具类型");
+        titleRow.createCell(2).setCellValue("家具用具名字");
+        titleRow.createCell(3).setCellValue("家具用具品牌");
+        titleRow.createCell(4).setCellValue("家具用具余量");
+        titleRow.createCell(5).setCellValue("所属公司");
+        int cell=1;
+        for(Furniture furniture:list){
+            Row row=sheet.createRow(cell);
+            row.createCell(0).setCellValue(furniture.getFurniture_id());
+            row.createCell(1).setCellValue(furniture.getFurniture_type());
+            row.createCell(2).setCellValue(furniture.getFurniture_name());
+            row.createCell(3).setCellValue(furniture.getFurniture_brand());
+            row.createCell(4).setCellValue(furniture.getFurniture_count());
+            row.createCell(5).setCellValue(furniture.getFurniture_company());
+            cell++;
+        }
+        return xssfWorkbook;
     }
 }
